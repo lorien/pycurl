f4baba016a9f40ac1c1c68f61641ca0a9b72494b
Gregory Petukhov <lorien@lorien.name>
2017-02-01 12:33:53 +0000

    Start refactoring winbuild.py

---
+++ win.py
@@ -0,0 +1,31 @@
+config = dict(
+    # whether to link libcurl against zlib
+    use_zlib = True,
+
+    # which version of zlib to use, will be downloaded from internet
+    zlib_version = '1.2.8',
+
+    # whether to use openssl instead of winssl
+    use_openssl = True,
+
+    # which version of openssl to use, will be downloaded from internet
+    openssl_version = '1.1.0c',
+
+    # whether to use c-ares
+    use_cares = True,
+
+    # FIX: which version of cares to use, will be downloaded from internet
+    cares_version = '1.12.0',
+
+    # whether to use libssh2
+    use_libssh2 = True,
+
+    # FIX: which version of libssh2 to use, will be downloaded from internet
+    libssh2_version = '1.8.0',
+
+    # which version of libcurl to use, will be downloaded from internet
+    libcurl_version = '7.51.0'
+
+    # virtualenv version
+    virtualenv_version = '15.1.0',
+)
